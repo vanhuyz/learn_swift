7ca4bac766581af92c1d1e1a2f74a3990af1a62d
Van Huy <vanphu.quanghuy@glpgs.com>
2015-12-02 18:53:09 +0900

    Resolve git conflicts

---
+++ Downloading Web Content/Downloading Web Content/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Downloading Web Content
+//
+//  Created by Van Huy on 12/2/15.
+//  Copyright (c) 2015 Example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://www.stackoverflow.com")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {
+            (data, response, error) in
+            
+            if error == nil {
+                var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding)
+                
+                println(urlContent)
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.webView.loadHTMLString(urlContent! as String, baseURL: nil)
+                }
+            }
+        }
+        
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
